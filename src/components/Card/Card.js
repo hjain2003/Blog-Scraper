b070682c9c0c01a1c418fb591fd47ccda7c6f6ef
Harsh <hjain_be21@thapar.edu>
2023-02-10 03:09:14 +0530

    added card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <div id="card_wrapper">
+        <div id="img">
+            IMAGE
+        </div>
+        <div id="card_text">
+            <b>THIS IS HEADING</b>
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam minus ab maxime sit sequi, asperiores perferendis blanditiis tempore quod possimus.
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Card
--- src/components/Latest/Latest.js
+++ src/components/Latest/Latest.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Card from '../Card/Card'
+import '../Card/Card.css'
 
 const Explore = () => {
   return (
     <div id="latest_wrapper">
         <h1 align="center">LATEST BLOGS</h1>
         <h2 align="center">Get your latest blogs here</h2>
+        <br /><br />
+        <Card/>
     </div>
   )
 }
